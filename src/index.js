796681a3c2462120dd68bd2d6093ed3a204424fb
Unes bg <mrgeray00@gmail.com>
2023-07-22 17:32:11 +0330

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-import './index.css'
-
-const root = ReactDom.createRoot(document.getElementById('root'))
-root.render(<App />)
-
